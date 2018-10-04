dc803c284481637e5da6ae28ad0c790fe7abb915
Håvard Bjørnøy <havard.bjornoy@gmail.com>
2018-10-04 11:03:45 +0200

    ignore datafolder and fix download-script

--- download_data.sh
+++ download_data.sh
@@ -4,19 +4,19 @@
 mkdir -p data/cnn_clean/cnn_stories_tokenized
 mkdir -p data/cnn_clean/dm_stories_tokenized
 
-printf "Downloading tokenized CNN-data and unzip it in data/cnn_clean/cnn_stories_tokenized/"
+printf "Downloading tokenized CNN-data and unzip it in data/cnn_clean/cnn_stories_tokenized/\n"
 printf "Downloaded from https://github.com/JafferWilson/Process-Data-of-CNN-DailyMail\n"
 curl -c /tmp/cookies "https://drive.google.com/uc?export=download&id=0BzQ6rtO2VN95cmNuc2xwUS1wdEE" > /tmp/intermezzo.html
 curl -L -b /tmp/cookies "https://drive.google.com$(cat /tmp/intermezzo.html | grep -Po 'uc-download-link" [^>]* href="\K[^"]*' | sed 's/\&amp;/\&/g')" > cnn_stories_tokenized.zip
 printf "\nUnzipping..."
 unzip -o cnn_stories_tokenized.zip -d data/cnn_clean/ | awk 'BEGIN {ORS=" "} {if(NR%1000==0)print "."}'
 
-printf "\nDownloading tokenized DailyMail-data and unzip it in data/cnn_clean/dm_stories_tokenized/"
+printf "\nDownloading tokenized DailyMail-data and unzip it in data/cnn_clean/dm_stories_tokenized/\n"
 printf "Downloaded from https://github.com/JafferWilson/Process-Data-of-CNN-DailyMail\n"
 curl -c /tmp/cookies "https://drive.google.com/uc?export=download&id=0BzQ6rtO2VN95bndCZDdpdXJDV1U" > /tmp/intermezzo.html
 curl -L -b /tmp/cookies "https://drive.google.com$(cat /tmp/intermezzo.html | grep -Po 'uc-download-link" [^>]* href="\K[^"]*' | sed 's/\&amp;/\&/g')" > dm_stories_tokenized.zip
 printf "\nUnzipping...\n"
-unzip -o cnn_stories_tokenized.zip -d data/cnn_clean/ | awk 'BEGIN {ORS=" "} {if(NR%1000==0)print "."}'
+unzip -o dm_stories_tokenized.zip -d data/cnn_clean/ | awk 'BEGIN {ORS=" "} {if(NR%1000==0)print "."}'
 
 # remove zip-files
 printf "\nRemoving zip-files\n"
